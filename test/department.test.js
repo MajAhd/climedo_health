6d07f2d49545f1da9edb44d7f0bb8130e3780942
Majid Ahmaditabar <mjd.ahd.tbr@gmail.com>
2021-09-24 18:42:27 +0330

    Department test done

--- test/department.test.js
+++ test/department.test.js
@@ -20,4 +20,19 @@ describe("Integration tests", () => {
     expect(department_delete).to.be.an("object");
     expect(department_delete.result).to.be.an("boolean");
   });
+
+  it(" new department + contact info", async () => {
+    let department = new Department();
+    let new_department = await department.new_department("oncology with contact", "api-key-sample");
+    expect(new_department).to.be.an("object");
+    expect(new_department.result).to.be.an("boolean");
+
+    let dep_contact = await department.update_department_contct(new_department.data.id, "c name", "c email", "c phone");
+    expect(dep_contact).to.be.an("object");
+    expect(dep_contact.result).to.be.an("boolean");
+
+    let department_delete = await department.delete_department(new_department.data._id);
+    expect(department_delete).to.be.an("object");
+    expect(department_delete.result).to.be.an("boolean");
+  });
 });
