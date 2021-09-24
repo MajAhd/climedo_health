d8f3e8b62ae80398122ca6bd73c4eb7a7ad682e3
Majid Ahmaditabar <mjd.ahd.tbr@gmail.com>
2021-09-24 18:25:30 +0330

    Department search added

--- routes/department.route.js
+++ routes/department.route.js
@@ -8,6 +8,8 @@ const DepartmentrUrl = process.env.API_V1 + "department";
 router.get(DepartmentrUrl, DepartmentController.get_all_departments);
 router.post(DepartmentrUrl, DepartmentController.new_department);
 router.get(DepartmentrUrl + "/:id", DepartmentController.get_department);
+
+router.get(DepartmentrUrl + "/search/:name", DepartmentController.search_department);
 router.post(DepartmentrUrl + "/:id", DepartmentController.update_department);
 router.delete(DepartmentrUrl + "/:id", DepartmentController.delete_department);
 
